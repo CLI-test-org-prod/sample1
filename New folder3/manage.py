a5ddc0ec3f349d507ec39bafa795a1813fa3aed5
prudhvinadh007 <prudhvinadh007@risksek.com>
2024-09-20 18:12:37 +0530

    yessss

--- New folder3/manage.py
+++ New folder3/manage.py
@@ -3,8 +3,3 @@ print("this is test-09 folder")
 print("this is test-09 folder")
 print("this is test-09 folder")
 print("this is test-09 folder") 
-
-firebaseApiKey="AAAA5678901:eyJ1Ijoiam9objU2Nzg2IiwiYSI6ImNsdnVzZjY5azFtbGUycW14N2JvNW10NWMifQhgvcjhdgydtewuytqsgybbhvhccfyuhjinkbvvccrttrdtykfyutyccjytyctycyxrzxdfcscv"
-
-AWS_IAM_ID="AKIA1234567890123456"
-awsSecretAccessKey="Aqdsdgr1234567890123456AKIA1234567890123456"
